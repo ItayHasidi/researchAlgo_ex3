b88a5d2748431d9c6afeb816948808ded7ab8b85
ItayHasidi <itayhasidi@gmail.com>
2022-11-14 16:31:44 +0200

    Update q3.py

--- q3.py
+++ q3.py
@@ -8,22 +8,20 @@ class List(list):
             list_index = self.data[index]
         else:
             for i in index:
-                print(type(list_index), type("int"))
-                if type(list_index) != type(int) and len(list_index) == 0:
-                    list_index = self.data[i]
-                else:
-                    list_index = list_index[i]
+                if not isinstance(list_index, int):
+                    if len(list_index) == 0:
+                        list_index = self.data[i]
+                    else:
+                        list_index = list_index[i]
         return list_index
 
     def __setitem__(self, index, value: object) -> None:  # operator[] for writing
-        # list_index = []
-        # for i in index:
-        #     if len(list_index) == 0:
-        #         list_index = self.data[i]
-        #     else:
-        #         list_index = list_index[i]
-        # list_index = value
-        self.data[index] = value
+        i0: int = index[0]
+        i1: int = index[1]
+        i2: int = index[2]
+        print(type(self.data))
+        print(self.data[i0, i1, i2])
+        self.data[index[0], index[1], index[2]] = value
 
 
 if __name__ == '__main__':
@@ -31,5 +29,8 @@ if __name__ == '__main__':
         [[[1, 2, 3, 33], [4, 5, 6, 66]], [[7, 8, 9, 99], [10, 11, 12, 122]], [[13, 14, 15, 155], [16, 17, 18, 188]], ])
     print(mylist[0, 1, 3])
     print(mylist[0])
-    mylist[0] = 88
+    # mylist[0] = 88
+    mylist[0, 1, 3] = 88
     print(mylist[0, 1, 3])
+    print(mylist.data)
+
