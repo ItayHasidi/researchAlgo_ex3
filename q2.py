7eefc39e84b20880a1a31e08c033e298af4f2d42
ItayHasidi <itayhasidi@gmail.com>
2022-11-14 20:27:07 +0200

    Update q2.py
    added tests to q2

--- q2.py
+++ q2.py
@@ -1,7 +1,33 @@
+import string
+
 history: dict = {}
 
 
 def lastcall(func):
+    """
+    A decorator function that remembers all previous calls, and if a call was already made the function will give
+    the answer without calling the original function again.
+
+    >>> f(10)
+    100
+    >>> f(10)
+    I already told you that the answer is 100!
+    >>> f(1)
+    1
+    >>> f(5)
+    25
+    >>> f(5)
+    I already told you that the answer is 25!
+    >>> f(1)
+    I already told you that the answer is 1!
+    >>> g("hello")
+    5
+    >>> g("hello")
+    I already told you that the answer is 5!
+    >>> g("HELLO")
+    5
+
+    """
     def wrap(*args, **kwargs):
         for val in args:
             if val in history.keys():
@@ -21,6 +47,11 @@ def lastcall(func):
 @lastcall
 def f(x: int):
     return x ** 2
+
+@lastcall
+def g(x: string):
+    return len(x)
+
 
 
 if __name__ == '__main__':
